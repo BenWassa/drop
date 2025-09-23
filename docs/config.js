bad4af469ca9d3c23b62d372b490df2efed28552
Benjamin Haddon <benjamin.haddon@ontariohealth.ca>
2025-09-23 16:10:44 -0400

    feat: Update SCRIPT_URL in APP_CONFIG to new Google Apps Script endpoint

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,7 @@
 // Configure the deployed Google Apps Script URL and API key
 // Example SCRIPT_URL: https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec
 window.APP_CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/YOUR_NEW_SCRIPT_ID_HERE/exec',
+  SCRIPT_URL: 'https://script.google.com/1XaRrwasutKZFvkNnEkLtsXWfIxe-pBD1MyQek-ElUYs/exec',
   API_KEY: '1QnI2H56tNySDZjLAKsiAAvkp7RzzvAUnfs-Hcsf6Z4S1PLgX5_HXTkQk',
   SYNC_INTERVAL_MS: 1000 * 60 * 1 // auto-sync every 1 minute when online
 };
