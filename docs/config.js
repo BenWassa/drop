ee5b480f8a4a6e42d407d57a33e8b5fd0e27c1ee
Benjamin Haddon <benjamin.haddon@ontariohealth.ca>
2025-10-02 09:31:02 -0400

    overhaul previous versions for v.2.2

--- docs/config.js
+++
@@ -1,6 +0,0 @@
-// Configure the deployed Google Apps Script URL and API key
-window.APP_CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyQay9hZK16ciNu6TP1-3NTiuTKOYLWsC6_9MNAl9iyiV4zETVG0B-8IMTYdjtYNMqz/exec',
-  API_KEY: 'AKfycbyQay9hZK16ciNu6TP1-3NTiuTKOYLWsC6_9MNAl9iyiV4zETVG0B-8IMTYdjtYNMqz',
-  SYNC_INTERVAL_MS: 1000 * 60 * 1 // auto-sync every 1 minute when online
-};
