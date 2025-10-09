5fc26c1ea527aeff8c96597be41abc58b0b387d3
Benjamin Haddon <benjamin.haddon@ontariohealth.ca>
2025-10-09 15:36:09 -0400

    fix: update service worker version to clear cache

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Service Worker for drop PWA
 
-const APP_VERSION = '3.0.0';
+const APP_VERSION = '3.0.1';
 const CACHE_NAME = `drop-cache-v${APP_VERSION.replace(/\./g, '-')}`;
 const urlsToCache = [
   './',
