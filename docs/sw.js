c8a83a5df396d6ef77741ba130fe7629ccb748fe
Benjamin Haddon <benjamin.haddon@ontariohealth.ca>
2025-10-23 14:05:24 -0400

    Update app version to 3.3.5 in sw.js and index.html

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Service Worker for drop PWA
 
-const APP_VERSION = '3.3.4';
+const APP_VERSION = '3.3.5';
 const CACHE_NAME = `drop-cache-v${APP_VERSION.replace(/\./g, '-')}`;
 const urlsToCache = [
   './',
