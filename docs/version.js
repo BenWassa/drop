4aa8305bca767546bb3f736865e4c10760b63bed
BenWassa <haddonbenjamin@gmail.com>
2025-12-29 14:20:28 -0500

    fix(sw): use self.APP_VERSION for service worker global scope

--- docs/version.js
+++ docs/version.js
@@ -5,3 +5,6 @@ export const APP_VERSION = '4.0.0';
 if (typeof window !== 'undefined') {
   window.APP_VERSION = APP_VERSION;
 }
+if (typeof self !== 'undefined') {
+  self.APP_VERSION = APP_VERSION;
+}
